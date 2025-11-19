78e051575a152a402724b8dfe3851f8371b3d3f1
Ju Smile <ju.smilespace@gmail.com>
2025-11-19 12:33:42 +0800

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById('root')!;
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// 註冊 service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/ju-smile-app/sw.js')
+      .catch((err) => console.error('SW registration failed', err));
+  });
+}
