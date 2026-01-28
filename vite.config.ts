50a00e8354428b9c4190188d1ded2e938fc681f9
Ju Smile <ju.smilespace@gmail.com>
2026-01-28 13:24:26 +0800

    修改src

--- vite.config.ts
+++ vite.config.ts
@@ -26,12 +26,12 @@ export default defineConfig(({ command }) => ({
         scope: process.env.CAPACITOR ? '/' : '/ju-smile-app/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'icons/ju-smile-icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icons/ju-smile-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
